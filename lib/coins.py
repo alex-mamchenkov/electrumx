0064d0d696f702a5b3865aad713f89ce6d6fd5b0
Alexander Mamchenkov <mamtchenkov@gmail.com>
2018-01-30 18:20:48 +0200

    Another test

--- lib/coins.py
+++ lib/coins.py
@@ -605,20 +605,6 @@ class MFCoin(Coin):
     HEADER_HASH = None
     DESERIALIZER = lib_tx.DeserializerTxTime
 
-    @classmethod
-    def header_hash(cls, header):
-        '''Given a header return the hash.'''
-        if cls.HEADER_HASH is None:
-            import scrypt
-            cls.HEADER_HASH = lambda x: scrypt.hash(x, x, 1024, 1, 1, 32)
-
-        version, = struct.unpack('<I', header[:4])
-        if version > 6:
-            return super().header_hash(header)
-        else:
-            return cls.HEADER_HASH(header)
-
-
 class LitecoinTestnet(Litecoin):
     SHORTNAME = "XLT"
     NET = "testnet"
