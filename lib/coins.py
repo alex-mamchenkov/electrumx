333f4d542b56bbd52d8ffd3bcb48b1339b3eff20
Alexander Mamchenkov <mamtchenkov@gmail.com>
2018-01-30 18:12:46 +0200

    More testing

--- lib/coins.py
+++ lib/coins.py
@@ -597,7 +597,8 @@ class MFCoin(Coin):
     NET = "mainnet"
     XPUB_VERBYTES = bytes.fromhex("0488b21e")
     XPRV_VERBYTES = bytes.fromhex("0488ade4")
-    P2SH_VERBYTES = [bytes.fromhex("05")]
+    P2PKH_VERBYTE = bytes.fromhex("30")
+    P2SH_VERBYTES = [bytes.fromhex("32"), bytes.fromhex("05")]
     GENESIS_HASH = '5ca17b85ce10702fd5ce8f63a5657905ae8ba41c7b7a886e8d13b317386dda06'
     TX_COUNT = 50908
     TX_COUNT_HEIGHT = 93765
